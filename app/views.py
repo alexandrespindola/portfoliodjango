b78ffb27831ccd319d600b0360d343fe95314f53
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-02-08 12:24:25 +0100

    Atualizações de código e recursos adicionados

--- app/views.py
+++ app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Case
 
 def index(request):
-    return render(request, 'app/index.html')
+    cases = Case.objects.all()
+    return render(request, 'app/index.html', {'cases': cases})
+
+# from django.shortcuts import render
+
+# def index(request):
+#     return render(request, 'app/index.html')
